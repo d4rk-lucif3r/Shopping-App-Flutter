7fa032f24f04c8d18d8b28aaa13e28a702f5fd08
Arsh Anwar <lucifer78908@gmail.com>
2020-12-10 18:55:45 +0530

    Fixed Passing of Arguments Between states
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+// import './widgets/banner.dart';
+import './providers/products_provider.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
-import './widgets/banner.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,50 +14,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shopping App',
-      theme: ThemeData(
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProviders(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shopping App',
+        theme: ThemeData(
           backgroundColor: Colors.black,
           canvasColor: Colors.black,
           primarySwatch: Colors.red,
           accentColor: Colors.amberAccent,
-          fontFamily: 'Lato'),
-      initialRoute: '/',
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case '/':
-            return CupertinoPageRoute(builder: (_) => ProductsOverviewScreen());
-          case ProductDetailScreen.routeName:
-            return CupertinoPageRoute(builder: (_) => ProductDetailScreen());
-          default:
-            return CupertinoPageRoute(builder: (_) => ProductsOverviewScreen());
-        }
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return wrapWithBanner(Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+          fontFamily: 'Lato',
         ),
-        body: Center(
-          child: Text(
-            '',
-          ),
-        )));
+        initialRoute: '/',
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case '/':
+              return CupertinoPageRoute(
+                  builder: (_) => ProductsOverviewScreen(), settings: settings);
+            case ProductDetailScreen.routeName:
+              return CupertinoPageRoute(builder: (_) => ProductDetailScreen(),settings: settings);
+            default:
+              return CupertinoPageRoute(
+                  builder: (_) => ProductsOverviewScreen(), settings: settings);
+          }
+        },
+      ),
+    );
   }
 }
