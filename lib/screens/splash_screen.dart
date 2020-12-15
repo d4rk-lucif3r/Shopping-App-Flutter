0ab53e75cf089527cd79d672d8d0f96bc9a7ef8c
Arsh Anwar <lucifer78908@gmail.com>
2020-12-16 00:34:41 +0530

    Added Splash Screen
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,7 +5,10 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Loading...'),
+        child: Text(
+          'Loading...',
+          style: Theme.of(context).textTheme.headline5,
+        ),
       ),
     );
   }
